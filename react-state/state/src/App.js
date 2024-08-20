124c8e90e473bfc54c849c20d6e037409f5541e3
Kamalpuriya <2322185@cognizant.com>
2024-08-20 14:57:02 +0530

    added project

---
+++ react-state/state/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+
+function App() {
+
+  const [data,setData]=useState(0);
+  function updateData(){
+    setData(data+1)
+  }
+  return (
+    <div className="App">
+      <h1>{data}</h1>
+      <button onClick={updateData}>Update Data</button>
+      
+    </div>
+  );
+}
+
+export default App;
